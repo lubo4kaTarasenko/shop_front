8645aa3fb95d5c39dc0f8c139e104d390fbff2f7
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2021-01-21 00:17:46 +0200

    sync authentication

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import {AppBar,Toolbar, Button, TextField, Grid, IconButton} from '@material-ui/core';
+import {AppBar,Toolbar,  TextField, Grid, IconButton} from '@material-ui/core';
 import React from 'react';
 import { Link } from "react-router-dom";
 //import cookie from 'react-cookies'
@@ -17,36 +17,27 @@ export default class Header extends React.Component {
             <Grid item xs={3} md={2} lg={5} className='header_item'> <h2>Let it be shop</h2></Grid> 
             <Grid item xs={9} md={4} lg={3} className='header_item'><SearchComp/></Grid> 
             <Grid item xs={6} md={3} lg={2} className='header_item'><h4>login as blabla</h4></Grid>
-            <Grid item xs={6} md={3} lg={2} className='header_item'>{ this.renderIfAuth(token) }</Grid> 
+            <Grid item xs={6} md={3} lg={2} className='header_item'>{ this.renderIcons() }</Grid> 
           </Grid>
         </Toolbar>
    </AppBar>
    )
  }
- renderIfAuth(token){
-  if (!token){
-    return( 
-        <div className='header'> 
-          <Button variant="contained" color='primary'><Link to="/sign_up">Sign up</Link></Button>
-            &nbsp;&nbsp;
-          <Button variant="contained" color='primary'><Link to="/log_in">Log in</Link></Button>          
-        </div>               
-  )}
-  else{
+ renderIcons(){  
     return(
           <div className='header'>   
             <Cart/> 
             &nbsp;&nbsp; 
-            <IconButton><Link to="/profile"><AccountBox/></Link></IconButton>
+            <IconButton><a href='http://localhost:3001/users/sign_in'><AccountBox/></a></IconButton>
             &nbsp;&nbsp;
             <IconButton><Link to="/home"><Home/></Link></IconButton>
           </div>
-  )}
-} 
-searchForm(){
-  return(
-  <span className='search_cont'><TextField label={ <Search id='search_i'/>} variant="filled" id='search_input' /></span>
   )
-}
+  } 
+  searchForm(){
+    return(
+    <span className='search_cont'><TextField label={ <Search id='search_i'/>} variant="filled" id='search_input' /></span>
+    )
+  }
    
 }

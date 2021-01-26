bd67bfd55e2c33673de0731c070950d2f714132b
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2021-01-26 16:38:22 +0200

    style comments

--- src/components/showAllComments.js
+++ src/components/showAllComments.js
@@ -6,14 +6,14 @@ import { Paper } from '@material-ui/core';
 
 export default function ShowAllComments(props) {  
   return (
-    <Paper>
+    <Paper style={{padding: '10px'}}>
       <h3 style={{textAlign: 'center'}}>Comments</h3>
       { props.comments.map(c =>                
-        <div key={c.id}>      
+        <div key={c.id} className='comment_cont'>      
           <Box component="fieldset" mb={3} borderColor="transparent">            
             <Rating name="read-only" value={c.rating} readOnly />
             <Typography>{c.body}</Typography>
-            <Typography><b style={{fontSize: 'x-small'}}>{c.email}</b></Typography>
+            <Typography><b style={{fontSize: 'small'}}>{c.email}</b></Typography>
           </Box> 
       </div> 
       )}    
